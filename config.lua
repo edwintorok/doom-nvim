50886be40fc984f3fd56c33d8a82735fd4ffd594
Edwin Török <edwin@etorok.net>
2022-05-22 15:28:19 +0100

    config(local): use comma as local leader
    Signed-off-by: Edwin Török <edwin@etorok.net>

--- config.lua
+++ config.lua
@@ -8,6 +8,8 @@ doom.preserve_edit_pos = true
 doom.relative_num = false
 
 vim.opt.guifont = 'GoMono Nerd Font:h19'
+
+vim.g.maplocalleader = ','
 -- ADDING A PACKAGE
 --
 -- doom.use_package("EdenEast/nightfox.nvim", "sainnhe/sonokai")
